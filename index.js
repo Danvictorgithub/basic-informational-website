a075d5e458bdaf8234778e660931f4190b063e3b
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2022-08-20 23:49:19 +0800

    fixed code to more flexible response

--- index.js
+++ index.js
@@ -1,49 +1,25 @@
 #!usr/bin/env node
-
 const http = require('http');
 const fs = require('fs');
-// const axios = require('axios');
-let htmlData;
-let about;
-let contact;
-let notFound;
-try {
-    const data = fs.readFileSync('./index.html', 'utf8');
-    htmlData = data;
-  } catch (err) {
-    console.error(err);
-  }
-try {
-    const data = fs.readFileSync('./about.html', 'utf8');
-    about = data;
-  } catch (err) {
-    console.error(err);
-  }
-  try {
-    const data = fs.readFileSync('./contact-me.html', 'utf8');
-    contact = data;
-  } catch (err) {
-    console.error(err);
-  }
-  try {
-    const data = fs.readFileSync('./404.html', 'utf8');
-    notFound = data;
-  } catch (err) {
-    console.error(err);
-  }
 const port = process.env.PORT || 8080;
 const server = http.createServer((req, res)=>{
     res.statusCode = 200;
     res.setHeader('Content-Type','text/html');
-    console.log(req);
-    if(req.url == '/') {
-        res.end(htmlData);
-    } else if (req.url == '/about'){
-        res.end(about);
-    } else if (req.url == '/contact') {
-        res.end(contact);
+    if (req.url == '/') {
+      try {
+        const data = fs.readFileSync('./index.html', 'utf8');
+        res.end(data);
+      } catch (err) {
+        res.end('error');
+      }
     } else {
-        res.end(notFound);
+      try {
+        const data = fs.readFileSync(`.${req.url}.html`, 'utf8');
+        res.end(data);
+      } catch (err) {
+        const data = fs.readFileSync(`./404.html`, 'utf8');
+        res.end(data);
+      }
     }
 });
 server.listen(port, ()=> {
